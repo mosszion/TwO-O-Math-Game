99d09328373fcc226408f4f2097b282009ef1e20
Mussie <musdzion@gmail.com>
2024-07-29 15:51:06 -0400

    add comment describing the main features of the game and MainGame class

---
+++ main.rb
@@ -0,0 +1,10 @@
+#Here will the MainGame class logic be implemented 
+#The MainGame class will control all the other class and require them as needed
+#The logis follows this steps: 
+# 1) Once the file run , there will be a prompt with a message to guess the addition of two random numbers
+# 2) Players will be let to type in thier guess and hit enter
+# 3) Then it will check if the players guess is correct 
+# 4) On correct answer it will print a success message while on miss a different message
+# 5) Players will contiue playing untill one player losses all three lives 
+# 6) Once reached to all loss stage , it will print the result and winner of the game
+
