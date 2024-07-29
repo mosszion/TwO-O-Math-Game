1fdc3a3eaffbfa4b082444e3c7c7c9b3949d3699
Mussie <musdzion@gmail.com>
2024-07-29 15:40:36 -0400

    add state and behavior that function the logic to the turn class

---
+++ turn.rb
@@ -0,0 +1,31 @@
+#Here Turn class is defined 
+#Turns class will require the question file to use the MathQuestion class
+#It will set the states of player and @questions from the MathQuestion class
+#It will define the method play which prompts a question and gets a players answer
+#Then it will check the answer and prints out a message either for success or not 
+#It will also call the lose_life if the answer is wrong, this will deduct one life from the lives of a player
+require_relative "question"
+
+class Turn 
+    def initialize(player)
+      @player = player
+      @question = MathQuestion.new
+    end
+    def play
+      puts @question.ask
+      answer = gets.chomp().to_i
+
+      if @question.check_answer(answer)
+        puts "#{@player}: Yes! You are correct."
+      elsif 
+        puts "#{@player}: Seriously? No!"
+        @player.lose_life
+      end
+    end
+
+end
+
+trialTurn = Turn.new("Mossi")
+
+puts trialTurn.play
+puts trialTurn.play
