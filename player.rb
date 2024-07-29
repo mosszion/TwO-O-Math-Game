18011dd3b0a53f4e241a676b3b767fd9182591d0
Mussie <musdzion@gmail.com>
2024-07-29 15:16:34 -0400

    add the state and behavior of Player class

--- player.rb
+++ player.rb
@@ -3,3 +3,17 @@
 #Defines lose_life method where when a player misses the correct answer losses life
 #Defines alive method where it checks how many lives has each player left with
 
+class Player 
+  attr_accessor :name, :lives
+  def initialize(name, lives = 3)
+      @name = name
+      @lives = lives
+
+  end
+  def lose_life
+      @lives -= 1
+  end 
+  def alive?
+      @lives > 0 
+  end
+end
