797f1981577486f98cad79bc29dcb222f1254ae9
Mussie <musdzion@gmail.com>
2024-07-29 15:07:32 -0400

    add variables and methods of question class

--- question.rb
+++ question.rb
@@ -6,3 +6,28 @@
 # And ask method is defined to return a prompt asking the addition of those rundom numbers
 #Also check_answer method is defined to check the answer taken from the players to the correct answer
 
+class MathQuestion
+    attr_accessor :num1, :num2, :correct_answer
+    def initialize 
+        @num1 = rand(1..20)
+        @num2 = rand(1..20)
+        @correct_answer = @num1 + @num2
+    end
+
+    def ask
+        "What does #{@num1} plus #{@num2} equal?"
+    end
+
+    def check_answer(answer)
+       answer == @correct_answer
+          
+    end
+
+end
+
+question1 = MathQuestion.new
+puts question1.ask
+puts question1.num1
+puts question1.num2
+puts question1.correct_answer
+puts question1.check_answer(5)
