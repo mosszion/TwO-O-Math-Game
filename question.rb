115c85c38d89cfab58bf1df063e2ebc86330f462
Mussie <musdzion@gmail.com>
2024-07-29 14:55:34 -0400

    add comments describing the class

---
+++ question.rb
@@ -0,0 +1,8 @@
+#This is a question file where the MathQuestion class is found
+#MathQuestion class responsiblity is to generate two random numbers below 20, ask thier addition and check the result 
+#This class initialized three variables i.e @num1, @num2 and @correct_answer
+#Randomly generated numbers will be saved into @num1 and @num2
+#While their addition will be placed inside the variable @current_answer
+# And ask method is defined to return a prompt asking the addition of those rundom numbers
+#Also check_answer method is defined to check the answer taken from the players to the correct answer
+
