cdebebacfe3d6772972a19702b516f95d8ab8c59
Mussie <musdzion@gmail.com>
2024-07-29 15:15:41 -0400

    add comment to the player class file

---
+++ player.rb
@@ -0,0 +1,5 @@
+#This is Player class which sets state nd behavior of players, check thier lives left
+#Initializes the state of name and lives(which lets every player to start with three lives)
+#Defines lose_life method where when a player misses the correct answer losses life
+#Defines alive method where it checks how many lives has each player left with
+
